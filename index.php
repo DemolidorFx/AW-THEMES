ec97cf85ee2b664f138a8d20a58c58ee849c33f7
DemolidorFx <99769245+DemolidorFx@users.noreply.github.com>
2022-05-04 00:33:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TEMA AW</title>
+</head>
+<body>
+	<h1>Olá mundo!!</h1>
+	<h2>Meu primeiro tema</h2>
+	<h3>By: Douglas Mattaüs Davide Miranda Policarpo</h3>
+</body>
+</html>
